2200c53e29c63c0ea3a1374e1cef295711990b43
Clement <clementclementson@gmail.com>
2023-12-11 22:29:50 +0400

    Refactored code for readability

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,23 +1,13 @@
-var counter = 0;
+var idCounter = 0;
 
 function addButtonClicked() {
-    let text = document.querySelector("#input-entry").value;
+    let text = document.querySelector("#todo-input").value;
     let ul = document.querySelector("ul");
 
-    let liId = "li" + counter;
-
-    let li = document.createElement("li");
-    li.setAttribute("id", liId);
-
-    let checkboxId = "checkbox" + counter;
-    
-    li.appendChild(createCheckbox(checkboxId, li));
-    li.appendChild(createLabel(checkboxId, text));
-    li.appendChild(createRemoveButton(li));
-    
+    li = createListItem(text);
     ul.appendChild(li);
 
-    counter = counter + 1;
+    document.querySelector("#todo-input").value = "";
 }
 
 function removeButtonClicked(element) {
@@ -32,42 +22,78 @@ function onChangeCheckbox(element) {
 
     if (input.checked) {
 
-        label.classList.add('completed');
+        label.classList.add('todo-item-label-completed');
     }
     else {
-        label.classList.remove('completed');
+        label.classList.remove('todo-item-label-completed');
     }
 }
 
+/*
+<li id=<li-id>>
+    <input id="<checkbox-id>" type="checkbox" class="todo-item-checkbox" onclick="onChangeCheckbox(<liid>)">
+    <label for="<checkbox-id>> {text} </label>
+    <a class="remove-item-button" onclick="removeButtonClicked(<li-id>)">
+        <i class='bx bx-x-circle'></i>
+    </a>
+</li>
+*/
+function createListItem(text) {
+    
+    let li = document.createElement("li");
+    
+    let liId = "li" + idCounter;
+    li.setAttribute("id", liId);
+
+    let checkboxId = "checkbox" + idCounter;
+    li.appendChild(createCheckbox(checkboxId, li));
+    li.appendChild(createLabel(checkboxId, text));
+    li.appendChild(createRemoveButton(li));
+
+    return li;
+}
+
+/*
+<input id="<checkbox-id>" type="checkbox" class="todo-item-checkbox" onclick="onChangeCheckbox(<liid>)">
+*/
 function createCheckbox(id, li) {
     let input = document.createElement("input");
     input.setAttribute("id", id);
     input.setAttribute("type", "checkbox");
-    input.setAttribute("class", "todo-entry-checkbox");
+    input.setAttribute("class", "todo-item-checkbox");
     input.setAttribute("onclick", "onChangeCheckbox(" + li.id + ")");
 
     return input;
 }
-
-function createLabel(forId, text) {
+/*
+<label for="<checkbox-id>> {text} </label>
+*/
+function createLabel(checkboxId, text) {
     let label = document.createElement("label");
-    label.setAttribute("for", forId);
-    label.setAttribute("class", "todo-entry-checkbox");
+    label.setAttribute("for", checkboxId);
     label.innerHTML = text;
 
     return label;
 }
 
+/*
+<a class="remove-item-button" onclick="removeButtonClicked(<li-id>)">
+    <i class='bx bx-x-circle'></i>
+</a>
+*/
 function createRemoveButton(li) {
     let anchor = document.createElement("a");
     anchor.setAttribute("class", "remove-item-button");
-
     anchor.setAttribute("onclick", "removeButtonClicked(" + li.id + ")");
 
     let icon = document.createElement("i");
     icon.setAttribute("class", "bx bx-x-circle");
 
     anchor.appendChild(icon);
-
     return anchor;
+}
+
+function updateIdCounter() {
+
+    idCounter++;
 }
